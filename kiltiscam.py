172bc184b34138322f54756f42d4bbf017be17e2
Tuomas Rantataro <tuomas.rantataro@aalto.fi>
2020-02-20 12:22:45 +0200

    Show screenshot time and climate data at kiltis

--- kiltiscam.py
+++ kiltiscam.py
@@ -1,5 +1,5 @@
 """
-Kiltiscam v1.0
+Kiltiscam v1.1
 
 A script to capture an image from the webcam in the guildroom
 of Inkubio. Original script got as an anniversary present from
@@ -18,7 +18,7 @@ Date: 29.9.2016
 
 
 from cv2 import *
-from time import sleep, time
+from time import sleep, time, strftime
 import requests
 import config  # Hidden parameters for image uploading to server
 
@@ -29,6 +29,54 @@ def camera_init():
     camera.set(CAP_PROP_FRAME_HEIGHT, 1080)
     return camera
 
+def read_gmw90_data():
+    """
+    Read data saved by another script monitoring climate of kiltis.
+    The file where to read data from is defined at config.py
+
+    Returns:
+        List of most recent tremperature, CO2 concentration and relative
+        humidity measurements.
+    """
+    measurements = []
+    with open("config.gmw90file", "r") as f:
+        for i in range(3):
+            meas = f.readline()
+            measurements.append(meas[:-1]) #removing trailing \n
+    return measurements
+
+def create_co2_color(co2_measurement):
+    """
+    Choose color of CO2 value text based on the value.
+    This is done to transform ppm values to more intuitive information.
+
+    Parameters:
+        co2_measurement: Amout of co2 in kiltis atmosphere (ppm)
+    Returns:
+        Tuple of BRG color value (because opencv likes BRG instead of RGB)
+    """
+    co2 = int(co2_measurement)
+    if co2 < 800:
+        blue = 200
+        green = 200
+        red = 200
+    elif co2 < 1100:
+        blue = int((1-((co2-800)/600))*200)
+        green = 200
+        red = 200
+    elif co2 < 1600:
+        blue = 0
+        green = int((1-((co2-1100)/600))*200)
+        red = 200
+    elif co2 < 2600:
+        blue = 0
+        green = 0
+        red = int((1-((co2-1600)/1000))*200)
+    else:
+        blue = 0
+        green = 0
+        red = 0
+    return (blue, green, red)
 
 def shoot(camera):
     """
@@ -58,6 +106,22 @@ def get_photo(camera, filename):
         nothing
     """
     img = shoot(camera)
+    meas = read_gmw90_data()
+
+    temp = str("Temp: {}C".format(meas[0]))
+    co2 = str("{}ppm".format(meas[1]))
+    hum = str("Humidity: {}%".format(meas[2]))
+
+    co2_int = int(meas[1])
+    co2color = create_co2_color(co2_int)
+
+    putText(img, strftime("%d.%m.%Y"), (20, 40), FONT_HERSHEY_DUPLEX, 1, (200, 200, 200), 1, LINE_AA)
+    putText(img, strftime("%H.%M.%S"), (20, 80), FONT_HERSHEY_DUPLEX, 1, (200, 200, 200), 1, LINE_AA)
+    putText(img, temp, (20, 980), FONT_HERSHEY_DUPLEX, 1, (200, 200, 200), 1, LINE_AA)
+    putText(img, 'CO2: ', (29, 1020), FONT_HERSHEY_DUPLEX, 1, (200, 200, 200), 1, LINE_AA)
+    putText(img, co2, (105, 1020), FONT_HERSHEY_DUPLEX, 1, co2color, 1, LINE_AA)
+    putText(img, hum, (20, 1060), FONT_HERSHEY_DUPLEX, 1, (200, 200, 200), 1, LINE_AA)
+
     imwrite("{}".format(filename), img)
     # Uncomment in case of kiltislapses
     # imwrite("/home/maikki/data1/kiltislapse/{}.jpg".format(time()), img)
@@ -99,7 +163,7 @@ def main():
             sleep(5)
         except Exception as e:
             with open("err.txt", "a") as f:
-                f.write("An error occurred:\n{}\n\n".format(e))
+                f.write("An error occurred on {}:\n{}\n\n".format(strftime("%H:%M:%S %d.%m.%Y"), e))
 
 
 if __name__ == "__main__":
